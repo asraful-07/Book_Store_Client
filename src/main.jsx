f8e326812783dd1a531c6d9c4a22cdd7bf2d4cf4
asraful-07 <asraful0508@gmail.com>
2025-07-28 17:47:37 +0600

    add reat toastify

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import Router from "./routes";
 import AuthProvider from "./provider/AuthProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-//
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
