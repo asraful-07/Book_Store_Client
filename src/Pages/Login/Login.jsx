c80f519f2b31c29c2b03918963344becbb1912c1
asraful-07 <asraful0508@gmail.com>
2025-07-28 18:08:26 +0600

    login done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../provider/AuthProvider";
 import { Link } from "react-router-dom";
@@ -10,7 +11,6 @@ import {
   FaEyeSlash,
   FaArrowRight,
 } from "react-icons/fa";
-import { toast } from "react-toastify/unstyled";
 
 const Login = () => {
   const {
