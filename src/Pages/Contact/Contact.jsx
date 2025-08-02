20b268e74f5f81f29dd40ab611a54389a808346f
asraful-07 <asraful0508@gmail.com>
2025-08-02 11:57:22 +0600

    add contatUs section

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactUs from "../../Components/ContactUs";
+import Map from "../../Components/Map";
 
 const Contact = () => {
   return (
     <div className="container mx-auto px-4 md:px-8">
+      <Map />
       <ContactUs />
     </div>
   );
